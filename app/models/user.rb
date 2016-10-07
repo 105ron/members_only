61c310ae1768712d2e87d12f1b30aafb2e9ae45d
rhys <rbrooker105@gmail.com>
2016-10-07 17:55:41 +1100

    user modelling through rails console. No forms

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+end
---
+++ db/migrate/20161007052015_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+    	t.string :name 
+    	t.string :email, unique: true
+    	t.string :password_digest
+    	t.string :remember_digest
+    	t.boolean :activated, default: false
+    	t.string :activation_digest
+    	t.datetime :activated_at
+      t.timestamps
+    end
+  end
+end
